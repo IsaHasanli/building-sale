8b65f527484a34fe8e768e021f870559f57bcad0
ilgarhasanzada <ilgarhasanzada@gmail.com>
2022-12-11 22:10:56 +0400

    umumi struktur quruldu

--- main/urls.py
+++ main/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', include('home.urls')),
+    path('property/', include('property.urls')),
+    path('gallery/', include('gallery.urls')),
+    path('blog/', include('blog.urls')),
+    path('contact/', include('contact.urls')),
+    path('account/', include('account.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
