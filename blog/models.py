f006b0efd92ab3fd6e5b1ce2013f684639a8f7ac
ilgarhasanzada <ilgarhasanzada@gmail.com>
2022-12-12 16:55:44 +0400

    blog appi elave edildi

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+User = get_user_model()
 
-# Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length = 50)
+    
+    def __str__(self) -> str:
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length = 100)
+    parent = models.ForeignKey('self',blank = True, null = True, on_delete = models.CASCADE, related_name = 'categories')
+    def __str__(self) -> str:
+        return f'{self.title} / {self.parent}'
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    image = models.ImageField(upload_to = 'blogs/', default = '../static/img/blog-img-1.jpg')
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name = 'blogs')
+    description = models.TextField()
+    description2 = models.TextField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'blogs')
+    tages = models.ManyToManyField(Tag,related_name = 'blogs', blank = True)
+    created_time = models.DateField(auto_now_add = True)
+    updated_time = models.DateField(auto_now = True)
+
+    def __str__(self) -> str:
+        return f'{self.title} / {self.category}'
+
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={"id": self.pk})
+    
+    def next_url(self):
+        for i in range(len(Blog.objects.all())):
+            if Blog.objects.all()[i].pk == self.pk and Blog.objects.last().pk != self.pk:
+                page=Blog.objects.all()[i+1].pk
+                return reverse("blog_detail", kwargs={"id": page})
+
+    def prev_url(self):
+        for i in range(len(Blog.objects.all())):
+            if Blog.objects.all()[i].pk == self.pk and Blog.objects.first().pk != self.pk:
+                page=Blog.objects.all()[i-1].pk
+                return reverse("blog_detail", kwargs={"id": page})
+
+    def has_next(self):
+        if Blog.objects.last().pk == self.pk:
+            return False
+        return True
+
+    def has_prev(self):
+        if Blog.objects.first().pk == self.pk:
+            return False
+        return True
+
+class Comment(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    website = models.CharField(max_length = 200)
+    content = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete = models.CASCADE, related_name = "comments")
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,null = True,  blank = True,  related_name = 'comments' )
+    created_time = models.DateField(auto_now_add = True)
+
+    def __str__(self) -> str:
+        return f'{self.blog} / {self.name} / {self.content}'
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.TextField()
+    image = models.ImageField('banner/')
+
+    def __str__(self) -> str:
+        return self.title
+    
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,54 @@
 from django.shortcuts import render
+from .models import Blog, Comment, Category, Tag, Banner
+from django.core.paginator import Paginator
+from django.db.models import Count, Sum
 
 def blog(request):
-    return render(request, 'blog.html')
+    search = request.GET.get('search')
+    category = request.GET.get('category')
+    tag = request.GET.get('tag')
+    author = request.GET.get('author')
+    if search:
+        blogs = Blog.objects.filter(title__contains = search).annotate(total_comments = Count('comments')).order_by("-id")
+        paginator = Paginator(blogs,8)
+        page_number = request.GET.get("page")
+        blog_obg = paginator.get_page(page_number)
+    elif author:
+        blogs = Blog.objects.filter(author = author).annotate(total_comments = Count('comments')).order_by("-id")
+        paginator = Paginator(blogs,8)
+        page_number = request.GET.get("page")
+        blog_obg = paginator.get_page(page_number)
+    elif category:
+        blogs = Blog.objects.filter(category = category).annotate(total_comments = Count('comments')).order_by("-id")
+        paginator = Paginator(blogs,8)
+        page_number = request.GET.get("page")
+        blog_obg = paginator.get_page(page_number)
+    elif tag:
+        blogs = Blog.objects.filter(tages = tag).annotate(total_comments = Count('comments')).order_by("-id")
+        paginator = Paginator(blogs,8)
+        page_number = request.GET.get("page")
+        blog_obg = paginator.get_page(page_number)
+    else:
+        blogs = Blog.objects.annotate(total_comments = Count('comments')).order_by("-id")
+        paginator = Paginator(blogs,8)
+        page_number = request.GET.get("page")
+        blog_obg = paginator.get_page(page_number)
+    categories = Category.objects.filter(parent = None).annotate(total_blogs = Count('blogs'), total_categories = Count('categories'))
+    alt_categories = Category.objects.exclude(parent = None).annotate(total_blogs = Count('blogs'),)
+    tages = Tag.objects.all()
+    recent_posts = Blog.objects.all().order_by('-id')[0:3]
+    banner = Banner.objects.first()
+    return render(request, 'blog.html', {"blog_obg": blog_obg, "categories": categories, "alt_categories": alt_categories, "tages": tages, "recent_posts" :recent_posts, "banner": banner})
+
+def blog_detail(request, id):
+    blog = Blog.objects.get(id = id)
+    categories = Category.objects.filter(parent = None).annotate(total_blogs = Count('blogs'), total_categories = Count('categories'))
+    alt_categories = Category.objects.exclude(parent = None).annotate(total_blogs = Count('blogs'),)
+    blog_tages = Tag.objects.filter(blogs = blog)
+    tages = Tag.objects.all()
+    recent_posts = Blog.objects.all().order_by('-id')[0:3]
+    banner = Banner.objects.first()
+    comments = Comment.objects.filter(blog = blog)
+    blogs_count = Blog.objects.count()
+    related_posts = Blog.objects.filter(category = blog.category).exclude(id=id).annotate(total_comments = Count('comments')).order_by("-id")
+    return render(request, 'blog-detail.html', {"blog": blog,"blogs_count":blogs_count, "related_posts": related_posts, "comments": comments,  "categories": categories, "alt_categories": alt_categories, "tages": tages, "blog_tages": blog_tages, "recent_posts" :recent_posts, "banner": banner})
